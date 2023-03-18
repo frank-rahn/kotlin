b0fa373173208e255e0114bc660894fa6fb929e0
Frank W. Rahn <frank@frank-rahn.de>
2023-03-18 14:46:07 +0100

    Anpassungen wegen neuer Kotlin Version und Erweiterung der Spring DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
-pluginManagement {
-  repositories {
-    maven {
-      url = uri("https://repo.spring.io/release")
-    }
-    gradlePluginPortal()
-  }
-}
-
 rootProject.name = "kotlin"
--- src/main/kotlin/de/rahn/kotlin/spring/KotlinApplication.kt
+++ src/main/kotlin/de/rahn/kotlin/spring/KotlinApplication.kt
@@ -5,29 +5,59 @@ import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.router
+import javax.sql.DataSource
 
 @SpringBootApplication
 class KotlinApplication
 
 data class Freelancer(var fistName: String? = null, val name: String)
 
+data class DBService(val db: DataSource)
+
 fun run(args: Array<String>) {
-    runApplication<KotlinApplication>(*args) {
-        addInitializers(
-            beans {
-                bean {
-                    router {
-                        GET("/freelancers") {
-                            ServerResponse.ok().body(arrayOf(Freelancer("Frank", "Rahn"), Freelancer("Martin", "Rahn")))
-                        }
-                        GET("/freelancers/{id}") {
-                            ServerResponse.ok().body(Freelancer(name = it.pathVariable("id")))
-                        }
-                    }
-                }
+  runApplication<KotlinApplication>(*args) {
+    addInitializers(
+      beans {
+        bean {
+          // Rest Server
+          router {
+            GET("/freelancers") {
+              ServerResponse.ok()
+                .body(arrayOf(Freelancer("Frank", "Rahn"), Freelancer("Martin", "Rahn")))
             }
-        )
-    }
+            GET("/freelancers/{id}") {
+              ServerResponse.ok().body(Freelancer(name = it.pathVariable("id")))
+            }
+          }
+        }
+//        bean {
+//          // Get a Service mit DB-Zugriff
+//          val db = ref<DataSource>()
+//          DBService(db)
+//        }
+//        bean {
+//          // Konfiguration: Websecurity
+//          val http = ref<HttpSecurity>()
+//          http {
+//            httpBasic {}
+//            authorizeRequests {
+//              authorize("/freelancers/**", hasAuthority("ROLE_USER"))
+//            }
+//          }.run { http.build() }
+//        }
+//        bean {
+//          // User
+//          InMemoryUserDetailsManager(
+//            User.withDefaultPasswordEncoder()
+//              .username("user")
+//              .password("password")
+//              .roles("USER")
+//              .build()
+//          )
+//        }
+      }
+    )
+  }
 }
 
 //fun main(args: Array<String>) = run(args)
